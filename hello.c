5b662595b3b841d44fb16207f2928d1dd88db134
Nelson Elhage <nelhage@nelhage.com>
2017-02-27 11:57:10 -0500

    Hello, World

---
+++ hello.c
@@ -0,0 +1,26 @@
+/**
+ * hello_rc -- a Hello, World kernel module for the Recurse Center
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nelson Elhage");
+MODULE_DESCRIPTION("Say Hello from RC");
+MODULE_VERSION("0.1");
+
+
+static int __init hello_rc_init(void){
+   printk(KERN_INFO "Hello, Recurse Center!\n");
+   return 0;
+}
+
+static void __exit hello_rc_exit(void){
+   printk(KERN_INFO "Never Graduate!\n");
+}
+
+
+module_init(hello_rc_init);
+module_exit(hello_rc_exit);
